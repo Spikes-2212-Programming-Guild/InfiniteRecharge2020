c120942c8d25d71ed0cfeac5513b1766d8b74dee
YuvalLevy1 <yuval124levy1@gmail.com>
2020-01-20 17:53:24 +0200

    created Turret subsystem

---
+++ src/main/java/com/spikes2212/frc2020/RobotMap.java
@@ -0,0 +1,13 @@
+package com.spikes2212.frc2020;
+
+public class RobotMap {
+
+    public interface CAN {
+        int TURRET_TALON = 0;
+    }
+
+    public interface DIO {
+        int TURRET_FIRST_LIMIT = 0;
+        int TURRET_SECOND_LIMIT = 0;
+    }
+}
---
+++ src/main/java/com/spikes2212/frc2020/subsystems/Turret.java
@@ -0,0 +1,79 @@
+package com.spikes2212.frc2020.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.spikes2212.command.genericsubsystem.GenericSubsystem;
+import com.spikes2212.control.PIDLoop;
+import com.spikes2212.control.PIDSettings;
+import com.spikes2212.dashboard.Namespace;
+import com.spikes2212.dashboard.RootNamespace;
+import com.spikes2212.frc2020.RobotMap;
+import edu.wpi.first.wpilibj.DigitalInput;
+
+import java.util.function.Supplier;
+
+public class Turret extends GenericSubsystem {
+
+    public static final Namespace NAMESPACE = new RootNamespace("Turret");
+
+
+    public static final Namespace PID = NAMESPACE.addChild("PID");
+
+    public static final Supplier<Double> MAX_SPEED = NAMESPACE.addConstantDouble("Max Speed", 0.6);
+    public static final Supplier<Double> MIN_SPEED = NAMESPACE.addConstantDouble("Min Speed", -0.6);
+
+    public static final Supplier<Double> K_P = PID.addConstantDouble("kP", 0);
+    public static final Supplier<Double> K_I = PID.addConstantDouble("kI", 0);
+    public static final Supplier<Double> K_D = PID.addConstantDouble("kD", 0);
+    public static final Supplier<Double> TOLERANCE = PID.addConstantDouble("Tolerance", 0);
+    public static final Supplier<Double> WAIT_TIME = PID.addConstantDouble("Wait Time", 0);
+
+    private static Turret instance;
+
+    public PIDLoop pidLoop;
+
+    PIDSettings pidSettings = new PIDSettings(K_P, K_I, K_D, TOLERANCE, WAIT_TIME);
+
+    private WPI_TalonSRX motor;
+    private DigitalInput firstLimit;
+    private DigitalInput secondLimit;
+
+    private Turret(WPI_TalonSRX motor, DigitalInput firstLimit, DigitalInput secondLimit) {
+        super(MIN_SPEED, MAX_SPEED);
+        this.motor = motor;
+        this.firstLimit = firstLimit;
+        this.secondLimit = secondLimit;
+
+//        pidLoop = new TalonPIDLoop(motor, pidSettings, MAX_SPEED);
+    }
+
+    public static Turret getInstance() {
+        if (instance == null) {
+            WPI_TalonSRX motor = new WPI_TalonSRX(RobotMap.CAN.TURRET_TALON);
+            DigitalInput firstLimit = new DigitalInput(RobotMap.DIO.TURRET_FIRST_LIMIT);
+            DigitalInput secondLimit = new DigitalInput(RobotMap.DIO.TURRET_SECOND_LIMIT);
+            instance = new Turret(motor, firstLimit, secondLimit);
+        }
+
+        return instance;
+    }
+
+    @Override
+    public void apply(double speed) {
+        motor.set(speed);
+    }
+
+    @Override
+    public boolean canMove(double speed) {
+        if (speed > 0)
+            return !firstLimit.get();
+        if (speed < 0)
+            return !secondLimit.get();
+
+        return true;
+    }
+
+    @Override
+    public void stop() {
+        motor.stopMotor();
+    }
+}
