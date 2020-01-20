e8f4666d315fe951ad335f80a76702f61809d931
YuvalLevy1 <yuval124levy1@gmail.com>
2020-01-20 17:53:24 +0200

    created Turret subsystem

--- src/main/java/com/spikes2212/frc2020/RobotMap.java
+++ src/main/java/com/spikes2212/frc2020/RobotMap.java
@@ -1,8 +1,15 @@
 package com.spikes2212.frc2020;
 
 public class RobotMap {
+
     public interface CAN {
-        int SHOOTER_MASTER = 0;
-        int SHOOTER_SLAVE = 1;
+        int TURRET_TALON = 5;
+        int SHOOTER_MASTER = 1;
+        int SHOOTER_SLAVE = 2;
+    }
+
+    public interface DIO {
+        int TURRET_FIRST_LIMIT = 0;
+        int TURRET_SECOND_LIMIT = 0;
     }
 }
