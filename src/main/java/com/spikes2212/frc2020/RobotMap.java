de5598c522bce810ee06728529f7a9ad957486c4
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2020-01-21 20:31:07 +0200

    added basic Intake and RobotMap

---
+++ src/main/java/com/spikes2212/frc2020/RobotMap.java
@@ -0,0 +1,14 @@
+package com.spikes2212.frc2020;
+
+public class RobotMap {
+    public interface PCM {
+        int LEFT_INTAKE_FORWARD = 4;
+        int LEFT_INTAKE_BACKWARD = 5;
+        int RIGHT_INTAKE_FORWARD = 2;
+        int RIGHT_INTAKE_BACKWARD = 3;
+    }
+
+    public interface PWM {
+        int INTAKE_MOTOR = 0;
+    }
+}
---
+++ src/main/java/com/spikes2212/frc2020/subsystems/Intake.java
@@ -0,0 +1,55 @@
+package com.spikes2212.frc2020.subsystems;
+
+import com.spikes2212.frc2020.RobotMap;
+import com.spikes2212.lib.command.genericsubsystem.GenericSubsystem;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.VictorSP;
+
+public class Intake extends GenericSubsystem {
+
+    public enum IntakeState {
+        UP, DOWN;
+    }
+
+    private DoubleSolenoid leftSolenoid, rightSolenoid;
+    private VictorSP motor;
+    private IntakeState state;
+
+    private static Intake instance;
+
+    public static Intake getInstance() {
+        if (instance == null) {
+            DoubleSolenoid left = new DoubleSolenoid(RobotMap.PCM.LEFT_INTAKE_FORWARD
+                    , RobotMap.PCM.LEFT_INTAKE_BACKWARD);
+            DoubleSolenoid right = new DoubleSolenoid(RobotMap.PCM.RIGHT_INTAKE_FORWARD,
+                    RobotMap.PCM.RIGHT_INTAKE_BACKWARD);
+            VictorSP motor = new VictorSP(RobotMap.PWM.INTAKE_MOTOR);
+            instance = new Intake(left, right, motor);
+        }
+        return instance;
+    }
+
+    private Intake(DoubleSolenoid left, DoubleSolenoid right, VictorSP motor) {
+        this.leftSolenoid = left;
+        this.rightSolenoid = right;
+        this.motor = motor;
+        this.state = IntakeState.UP;
+    }
+
+    @Override
+    public void apply(double speed) {
+        motor.set(speed);
+    }
+
+    @Override
+    public boolean canMove(double speed) {
+        if(state == IntakeState.UP)
+            return false;
+        return true;
+    }
+
+    @Override
+    public void stop() {
+        motor.stopMotor();
+    }
+}
