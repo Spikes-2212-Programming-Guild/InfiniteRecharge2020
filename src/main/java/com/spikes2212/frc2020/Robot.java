e0b11091af9cb4888b3cf4b884735c738a0c18bc
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2020-01-21 20:52:46 +0200

    added initTestingDashboard to robotInit

--- src/main/java/com/spikes2212/frc2020/Robot.java
+++ src/main/java/com/spikes2212/frc2020/Robot.java
@@ -1,11 +1,11 @@
 package com.spikes2212.frc2020;
 
-import edu.wpi.first.wpilibj.IterativeRobotBase;
+import com.spikes2212.frc2020.subsystems.Intake;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
     @Override
     public void robotInit() {
-
+        Intake.getInstance().initTestingDashboard();
     }
 }
