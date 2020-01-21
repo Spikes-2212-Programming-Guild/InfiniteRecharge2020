b3bdf3115eb3cc029c2a7778413f598856319abc
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2020-01-21 20:51:55 +0200

    added a namespace

--- src/main/java/com/spikes2212/frc2020/subsystems/Intake.java
+++ src/main/java/com/spikes2212/frc2020/subsystems/Intake.java
@@ -2,10 +2,22 @@ package com.spikes2212.frc2020.subsystems;
 
 import com.spikes2212.frc2020.RobotMap;
 import com.spikes2212.lib.command.genericsubsystem.GenericSubsystem;
+import com.spikes2212.lib.command.genericsubsystem.commands.MoveGenericSubsystem;
+import com.spikes2212.lib.dashboard.Namespace;
+import com.spikes2212.lib.dashboard.RootNamespace;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+import java.util.function.Supplier;
 
 public class Intake extends GenericSubsystem {
+
+    public static Namespace intakeNamespace = new RootNamespace("intake");
+
+    public static Supplier<Double> minSpeed;
+    public static Supplier<Double> maxSpeed;
+    public static Supplier<Double> gripSpeed;
 
     public enum IntakeState {
         UP, DOWN;
@@ -30,6 +42,7 @@ public class Intake extends GenericSubsystem {
     }
 
     private Intake(DoubleSolenoid left, DoubleSolenoid right, VictorSP motor) {
+        super(minSpeed, maxSpeed);
         this.leftSolenoid = left;
         this.rightSolenoid = right;
         this.motor = motor;
@@ -49,5 +62,23 @@ public class Intake extends GenericSubsystem {
     @Override
     public void stop() {
         motor.stopMotor();
+    }
+
+    public void initTestingDashboard() {
+        minSpeed = intakeNamespace.addConstantDouble("min speed", -1);
+        maxSpeed = intakeNamespace.addConstantDouble("max speed", 1);
+        gripSpeed = intakeNamespace.addConstantDouble("grip speed", 0.5);
+        intakeNamespace.putData("open", new InstantCommand(() -> {
+            state = IntakeState.DOWN;
+            leftSolenoid.set(DoubleSolenoid.Value.kForward);
+            rightSolenoid.set(DoubleSolenoid.Value.kForward);
+        }, this));
+        intakeNamespace.putData("close", new InstantCommand(() -> {
+            state = IntakeState.UP;
+            leftSolenoid.set(DoubleSolenoid.Value.kReverse);
+            rightSolenoid.set(DoubleSolenoid.Value.kReverse);
+        }, this));
+        intakeNamespace.putData("grip", new MoveGenericSubsystem(this, gripSpeed));
+        intakeNamespace.putString("state", state::name);
     }
 }
