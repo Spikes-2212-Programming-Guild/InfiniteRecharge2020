feacbb2564bce9d4e2ab509161fb56ca510e70e9
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2020-01-21 18:51:41 +0200

    removed compressor

--- src/main/java/com/spikes2212/frc2020/Robot.java
+++ src/main/java/com/spikes2212/frc2020/Robot.java
@@ -5,10 +5,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
 
-    public static Compressor compressor;
-
     @Override
     public void robotInit() {
-        compressor = new Compressor(RobotMap.CAN.PCM);
     }
 }
