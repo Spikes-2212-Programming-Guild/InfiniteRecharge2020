6c8127a46bcc7f34d3190e0d11401d432db63aa1
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2020-01-21 19:30:02 +0200

    added basic Drivetrain and RobotMap

---
+++ src/main/java/com/spikes2212/frc2020/RobotMap.java
@@ -0,0 +1,17 @@
+package com.spikes2212.frc2020;
+
+public class RobotMap {
+    public interface CAN {
+        int LEFT_TALON = 2;
+        int LEFT_VICTOR = 3;
+        int RIGHT_TALON = 4;
+        int RIGHT_VICTOR = 5;
+    }
+
+    public interface DIO {
+        int LEFT_ENCODER_POS = 0;
+        int LEFT_ENCODER_NEG = 1;
+        int RIGHT_ENCODER_POS = 2;
+        int RIGHT_ENCODER_NEG = 3;
+    }
+}
