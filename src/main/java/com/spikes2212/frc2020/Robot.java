462eeeb852bdafeceb30e1bc18db6b3c4ac0f2df
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2020-01-21 19:43:16 +0200

    added Joysticks and default command

--- src/main/java/com/spikes2212/frc2020/Robot.java
+++ src/main/java/com/spikes2212/frc2020/Robot.java
@@ -1,11 +1,17 @@
 package com.spikes2212.frc2020;
 
-import edu.wpi.first.wpilibj.IterativeRobotBase;
+import com.spikes2212.frc2020.subsystems.Drivetrain;
+import com.spikes2212.lib.command.drivetrains.commands.DriveArcade;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
+
+    private OI oi;
+
     @Override
     public void robotInit() {
-
+        oi = new OI();
+        Drivetrain.getInstance().setDefaultCommand(new DriveArcade(Drivetrain.getInstance(),
+                oi::getLeftY, oi::getRightX));
     }
 }
