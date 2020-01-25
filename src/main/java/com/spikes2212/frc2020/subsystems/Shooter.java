5dacd6a947a625368163a3c3c69a11e0fd6b9cc0
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2020-01-25 18:47:11 +0200

    made `distancePerPulse` a `double`

--- src/main/java/com/spikes2212/frc2020/subsystems/Shooter.java
+++ src/main/java/com/spikes2212/frc2020/subsystems/Shooter.java
@@ -17,11 +17,12 @@ public class Shooter extends GenericSubsystem implements TalonSubsystem {
     public static final Namespace shooterNamespace = new RootNamespace("Shooter");
     public static final Namespace PID = shooterNamespace.addChild("PID");
 
-    public static final Supplier<Double> distancePerPulse =
-            shooterNamespace.addConstantDouble("Distance per Pulse", 2 * 0.0254 * Math.PI / 2048);
+    public static final double distancePerPulse = 2 * 0.0254 * Math.PI / 2048;
 
     public static final Supplier<Double> maxSpeed = shooterNamespace.addConstantDouble("Max Speed", 0.6);
     public static final Supplier<Double> minSpeed = shooterNamespace.addConstantDouble("Min Speed", 0);
+    public static final Supplier<Double> shootSpeed =
+            shooterNamespace.addConstantDouble("Shooting Speed", 0.6);
 
     public static final Supplier<Double> kP = PID.addConstantDouble("kP", 0);
     public static final Supplier<Double> kI = PID.addConstantDouble("kI", 0);
@@ -94,7 +95,7 @@ public class Shooter extends GenericSubsystem implements TalonSubsystem {
         master.config_kD(loop.get(), pidSettings.getkD(), timeout.get());
         master.config_kF(loop.get(), kF.get(), timeout.get());
 
-        master.set(ControlMode.Velocity, setpoint / distancePerPulse.get());
+        master.set(ControlMode.Velocity, setpoint / distancePerPulse);
     }
 
     @Override
