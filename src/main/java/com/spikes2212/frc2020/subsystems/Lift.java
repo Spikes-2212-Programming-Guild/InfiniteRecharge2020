c11ad7cc1f75fcc8eb20a6eab2cc6da3a1e9dc56
talsi <tal.sitton04@gmail.com>
2020-01-25 14:01:54 +0200

    renamed to Lift

--- src/main/java/com/spikes2212/frc2020/subsystems/Lift.java
+++ src/main/java/com/spikes2212/frc2020/subsystems/Lift.java
@@ -12,8 +12,8 @@ import edu.wpi.first.wpilibj.Encoder;
 
 import java.util.function.Supplier;
 
-public class Elevator extends GenericSubsystem {
-    private static final Namespace namespace = new RootNamespace("Elevator");
+public class Lift extends GenericSubsystem {
+    private static final Namespace namespace = new RootNamespace("Lift");
     public static final Supplier<Double> distancePerPulse = namespace.addConstantDouble("Distance Per Pulse", 0);
     private static final Namespace pidNamespace = namespace.addChild("PID");
     private static final Supplier<Double> kP = pidNamespace.addConstantDouble("kP", 0);
@@ -25,22 +25,22 @@ public class Elevator extends GenericSubsystem {
     private static final Supplier<Double> kG = feedForwardNamespace.addConstantDouble("kG", 0);
     public static final FeedForwardSettings FEED_FORWARD_SETTINGS = new FeedForwardSettings(kS, () -> 0.0, () -> 0.0, kG);
 
-    private static Elevator instance;
+    private static Lift instance;
     private WPI_TalonSRX motor;
     private Encoder encoder;
     private DigitalInput bottomLimitSwitch = new DigitalInput(RobotMap.DIO.ELEVATOR_BOTTOM_SWITCH);
     private DigitalInput topLimitSwitch = new DigitalInput(RobotMap.DIO.ELEVATOR_TOP_SWITCH);
 
-    private Elevator(WPI_TalonSRX motor, Encoder encoder) {
+    private Lift(WPI_TalonSRX motor, Encoder encoder) {
         this.motor = motor;
         this.encoder = encoder;
     }
 
-    public static Elevator getInstance() {
+    public static Lift getInstance() {
         if (instance == null) {
             WPI_TalonSRX motor = new WPI_TalonSRX(RobotMap.CAN.ELEVATOR_TALON);
             Encoder encoder = new Encoder(RobotMap.DIO.ELEVATOR_ENCODER_A, RobotMap.DIO.ELEVATOR_ENCODER_B);
-            instance = new Elevator(motor, encoder);
+            instance = new Lift(motor, encoder);
         }
         return instance;
     }
