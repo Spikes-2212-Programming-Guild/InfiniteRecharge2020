28267ba6444c55c72d28818ea1902eb2f8478bfb
talsi <tal.sitton04@gmail.com>
2020-01-25 15:25:20 +0200

    changed lift to elevator, fixed can move function, and renamed initDashboard

--- src/main/java/com/spikes2212/frc2020/subsystems/Elevator.java
+++ src/main/java/com/spikes2212/frc2020/subsystems/Elevator.java
@@ -12,39 +12,39 @@ import edu.wpi.first.wpilibj.Encoder;
 
 import java.util.function.Supplier;
 
-public class Lift extends GenericSubsystem {
-    private static final Namespace liftNamespace = new RootNamespace("Lift");
-    public static final Supplier<Double> distancePerPulse = liftNamespace.addConstantDouble("Distance Per Pulse", 0);
-    private static final Namespace pidNamespace = liftNamespace.addChild("PID");
+public class Elevator extends GenericSubsystem {
+    private static final Namespace ElevatorNamespace = new RootNamespace("Elevator");
+    public static final Supplier<Double> distancePerPulse = ElevatorNamespace.addConstantDouble("Distance Per Pulse", 0);
+    private static final Namespace pidNamespace = ElevatorNamespace.addChild("PID");
     private static final Supplier<Double> kP = pidNamespace.addConstantDouble("kP", 0);
     private static final Supplier<Double> kI = pidNamespace.addConstantDouble("kI", 0);
     private static final Supplier<Double> kD = pidNamespace.addConstantDouble("kD", 0);
     public static final PIDSettings PID_SETTINGS = new PIDSettings(kP, kI, kD);
-    private static final Namespace feedForwardNamespace = liftNamespace.addChild("Feed Forward");
+    private static final Namespace feedForwardNamespace = ElevatorNamespace.addChild("Feed Forward");
     private static final Supplier<Double> kS = feedForwardNamespace.addConstantDouble("kS", 0);
     private static final Supplier<Double> kG = feedForwardNamespace.addConstantDouble("kG", 0);
     public static final FeedForwardSettings FEED_FORWARD_SETTINGS = new FeedForwardSettings(kS, () -> 0.0, () -> 0.0, kG);
 
-    private static Lift instance;
+    private static Elevator instance;
     private WPI_TalonSRX motor;
     private Encoder encoder;
     private DigitalInput bottomLimitSwitch;
     private DigitalInput topLimitSwitch;
 
-    private Lift(WPI_TalonSRX motor, Encoder encoder, DigitalInput bottomLimitSwitch, DigitalInput topLimitSwitch) {
+    private Elevator(WPI_TalonSRX motor, Encoder encoder, DigitalInput bottomLimitSwitch, DigitalInput topLimitSwitch) {
         this.motor = motor;
         this.encoder = encoder;
         this.bottomLimitSwitch = bottomLimitSwitch;
         this.topLimitSwitch = topLimitSwitch;
     }
 
-    public static Lift getInstance() {
+    public static Elevator getInstance() {
         if (instance == null) {
             WPI_TalonSRX motor = new WPI_TalonSRX(RobotMap.CAN.ELEVATOR_TALON);
             Encoder encoder = new Encoder(RobotMap.DIO.ELEVATOR_ENCODER_POS, RobotMap.DIO.ELEVATOR_ENCODER_NEG);
             DigitalInput bottomLimitSwitch = new DigitalInput(RobotMap.DIO.ELEVATOR_BOTTOM_SWITCH);
             DigitalInput topLimitSwitch = new DigitalInput(RobotMap.DIO.ELEVATOR_TOP_SWITCH);
-            instance = new Lift(motor, encoder, bottomLimitSwitch, topLimitSwitch);
+            instance = new Elevator(motor, encoder, bottomLimitSwitch, topLimitSwitch);
         }
         return instance;
     }
@@ -60,7 +60,7 @@ public class Lift extends GenericSubsystem {
 
     @Override
     public boolean canMove(double speed) {
-        return !(bottomLimitSwitch.get() && speed < 0) && !(topLimitSwitch.get() && speed > 0);
+        return !((bottomLimitSwitch.get() && speed < 0) && !(topLimitSwitch.get() && speed > 0));
     }
 
     @Override
@@ -68,9 +68,10 @@ public class Lift extends GenericSubsystem {
         motor.stopMotor();
     }
 
-    public void initDashboard() {
-        liftNamespace.putNumber("encoder", encoder::get);
-        liftNamespace.putBoolean("bottom limit switch", bottomLimitSwitch::get);
-        liftNamespace.putBoolean("top limit switch", topLimitSwitch::get);
+    @Override
+    public void configureDashboard() {
+        ElevatorNamespace.putNumber("encoder", encoder::get);
+        ElevatorNamespace.putBoolean("bottom limit switch", bottomLimitSwitch::get);
+        ElevatorNamespace.putBoolean("top limit switch", topLimitSwitch::get);
     }
 }
