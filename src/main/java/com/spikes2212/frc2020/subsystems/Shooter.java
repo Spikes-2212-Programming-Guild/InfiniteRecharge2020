5af8e14b53c83a55898e153470c30938f574c1e4
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2020-01-25 11:13:40 +0200

    removed `PIDLoop` usage

--- src/main/java/com/spikes2212/frc2020/subsystems/Shooter.java
+++ src/main/java/com/spikes2212/frc2020/subsystems/Shooter.java
@@ -2,42 +2,38 @@ package com.spikes2212.frc2020.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.spikes2212.command.genericsubsystem.GenericSubsystem;
-import com.spikes2212.control.*;
-import com.spikes2212.dashboard.Namespace;
-import com.spikes2212.dashboard.RootNamespace;
+import com.spikes2212.lib.command.genericsubsystem.GenericSubsystem;
+import com.spikes2212.lib.control.*;
+import com.spikes2212.lib.dashboard.Namespace;
+import com.spikes2212.lib.dashboard.RootNamespace;
 import com.spikes2212.frc2020.RobotMap;
 
 import java.util.function.Supplier;
 
 public class Shooter extends GenericSubsystem {
-    public static final Namespace NAMESPACE = new RootNamespace("Shooter");
-    public static final Namespace PID = NAMESPACE.addChild("PID");
+    public static final Namespace namespace = new RootNamespace("Shooter");
+    public static final Namespace PID = namespace.addChild("PID");
 
-    public static final Supplier<Double> MAX_SPEED = NAMESPACE.addConstantDouble("Max Speed", 0.6);
-    public static final Supplier<Double> MIN_SPEED = NAMESPACE.addConstantDouble("Min Speed", -0.6);
+    public static final Supplier<Double> maxSpeed = namespace.addConstantDouble("Max Speed", 0.6);
+    public static final Supplier<Double> minSpeed = namespace.addConstantDouble("Min Speed", -0.6);
 
-    public static final Supplier<Double> K_P = PID.addConstantDouble("kP", 0);
-    public static final Supplier<Double> K_I = PID.addConstantDouble("kI", 0);
-    public static final Supplier<Double> K_D = PID.addConstantDouble("kD", 0);
-    public static final Supplier<Double> TOLERANCE = PID.addConstantDouble("Tolerance", 0);
-    public static final Supplier<Double> WAIT_TIME = PID.addConstantDouble("Wait Time", 0);
+    public static final Supplier<Double> kP = PID.addConstantDouble("kP", 0);
+    public static final Supplier<Double> kI = PID.addConstantDouble("kI", 0);
+    public static final Supplier<Double> kD = PID.addConstantDouble("kD", 0);
+    public static final Supplier<Double> tolerance = PID.addConstantDouble("Tolerance", 0);
+    public static final Supplier<Double> waitTime = PID.addConstantDouble("Wait Time", 0);
 
     private static Shooter instance;
 
-    private PIDLoop pidForSpeed;
-
-    private PIDSettings pidSettings = new PIDSettings(K_P, K_I, K_D, TOLERANCE, WAIT_TIME);
+    private PIDSettings pidSettings = new PIDSettings(kP, kI, kD, tolerance, waitTime);
 
     private WPI_TalonSRX master;
     private WPI_TalonSRX slave;
 
     private Shooter(WPI_TalonSRX master, WPI_TalonSRX slave) {
-        super(MIN_SPEED, MAX_SPEED);
+        super(minSpeed, maxSpeed);
         this.master = master;
         this.slave = slave;
-
-        this.pidForSpeed = new TalonPIDLoop(master, pidSettings, this::canMove, MAX_SPEED, ControlMode.Velocity);
     }
 
     public static Shooter getInstance() {
@@ -65,9 +61,5 @@ public class Shooter extends GenericSubsystem {
     @Override
     public void stop() {
         master.stopMotor();
-    }
-
-    public PIDLoop getPIDForSpeed() {
-        return pidForSpeed;
     }
 }
