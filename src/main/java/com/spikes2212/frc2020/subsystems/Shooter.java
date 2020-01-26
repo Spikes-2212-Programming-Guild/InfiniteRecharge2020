a6394f21e058a58cc47e331869c03b70dc2ba499
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2020-01-26 20:35:12 +0200

    uncapitalized namespace

--- src/main/java/com/spikes2212/frc2020/subsystems/Shooter.java
+++ src/main/java/com/spikes2212/frc2020/subsystems/Shooter.java
@@ -14,7 +14,7 @@ import com.spikes2212.lib.dashboard.RootNamespace;
 import java.util.function.Supplier;
 
 public class Shooter extends GenericSubsystem implements TalonSubsystem {
-    public static final Namespace shooterNamespace = new RootNamespace("Shooter");
+    public static final Namespace shooterNamespace = new RootNamespace("shooter");
     public static final Namespace PID = shooterNamespace.addChild("PID");
 
     public static final Supplier<Double> MAX_SPEED = shooterNamespace.addConstantDouble("Max Speed", 0.6);
