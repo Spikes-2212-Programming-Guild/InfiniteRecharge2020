fa0da85034cc4c1b9c4b5ff1beb5a5cb01d5ad11
YuvalLevy1 <yuval124levy1@gmail.com>
2020-01-26 16:58:45 +0200

    made `VisionService` a singleton

--- src/main/java/com/spikes2212/frc2020/VisionService.java
+++ src/main/java/com/spikes2212/frc2020/VisionService.java
@@ -1,5 +1,7 @@
 package com.spikes2212.frc2020;
 
+import com.spikes2212.lib.dashboard.Namespace;
+import com.spikes2212.lib.dashboard.RootNamespace;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
@@ -7,14 +9,38 @@ import java.util.function.Supplier;
 
 public class VisionService {
 
-    private static final double targetHeight = 0;
+    private static final Namespace visionServiceNamespace = new RootNamespace("vision service");
+    private Supplier<Double> turretHeight = visionServiceNamespace.addConstantDouble("turret height", 78); //recheck the turret height
+    private Supplier<Double> retroReflectiveHeight = visionServiceNamespace.addConstantDouble("retro reflective height", 249 - turretHeight.get());
+    private Supplier<String> cameraName = visionServiceNamespace.addConstantString("camera name", "turretCam");
 
-    private static NetworkTable turretCam = NetworkTableInstance.getDefault().getTable("chameleon-vision").getSubTable("front");
+    public static VisionService visionService;
+    private NetworkTable turretCam;
+    private  Supplier<Double> yawToRetroReflective;
+    private  Supplier<Double> pitchToRetroReflective;
 
-    public static final Supplier<Double> yawToRetroReflective = () -> turretCam.getEntry("yaw").getDouble(0.0);
-    public static final Supplier<Double> pitchToRetroReflective = () -> turretCam.getEntry("pitch").getDouble(0.0);
-
-    public static double getDistanceToRetroReflective() {
-        return targetHeight / Math.tan(Math.abs(pitchToRetroReflective.get()));
+    public VisionService() {
+        turretCam = NetworkTableInstance.getDefault().getTable("chameleon-vision").getSubTable(cameraName.get());
+        yawToRetroReflective = () -> turretCam.getEntry("yaw").getDouble(0.0);
+        pitchToRetroReflective = () -> turretCam.getEntry("pitch").getDouble(0.0);
     }
+
+    public static VisionService getInstance() {
+        if (visionService == null)
+            visionService = new VisionService();
+        return visionService;
+    }
+
+    public double getYawToRetroReflective() {
+        return yawToRetroReflective.get();
+    }
+
+    public double getPitchToRetroReflective() {
+        return pitchToRetroReflective.get();
+    }
+
+    public double getDistanceToRetroReflective() {
+        return retroReflectiveHeight.get() / Math.tan(Math.abs(pitchToRetroReflective.get()));
+    }
+
 }
