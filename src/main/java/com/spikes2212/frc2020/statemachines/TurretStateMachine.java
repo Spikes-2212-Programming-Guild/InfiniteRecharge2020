f4606970da7330369a6db6260a603bf4f169ce78
YuvalLevy1 <yuval124levy1@gmail.com>
2020-02-02 21:11:59 +0200

    added drivetrain to class

--- src/main/java/com/spikes2212/frc2020/statemachines/TurretStateMachine.java
+++ src/main/java/com/spikes2212/frc2020/statemachines/TurretStateMachine.java
@@ -1,6 +1,7 @@
 package com.spikes2212.frc2020.statemachines;
 
 import com.spikes2212.frc2020.Robot;
+import com.spikes2212.frc2020.subsystems.Drivetrain;
 import com.spikes2212.frc2020.subsystems.Turret;
 import com.spikes2212.lib.command.genericsubsystem.commands.MoveGenericSubsystem;
 import com.spikes2212.lib.command.genericsubsystem.commands.MoveTalonSubsystem;
@@ -15,7 +16,9 @@ public class TurretStateMachine extends StateMachine<TurretStateMachine.TurretSt
     }
 
     private static TurretStateMachine instance;
+    private Drivetrain drivetrain = Drivetrain.getInstance();
     public static TurretStateMachine getInstance() {
+
         if (instance == null)
             instance = new TurretStateMachine();
         return instance;
@@ -32,7 +35,7 @@ public class TurretStateMachine extends StateMachine<TurretStateMachine.TurretSt
         addTransformation(TurretState.OFF, new InstantCommand(()-> turret.setEnabled(false)));
 //        addTransformation(TurretState.VISION, new MoveTalonSubsystem(turret,o,0));
         addTransformation(TurretState.MANUAL, new MoveTalonSubsystem(turret, Robot.oi::getRightX, () -> 0.0).perpetually()); //should be something else from OI
-        addTransformation(TurretState.ABSOLUTE, new MoveTalonSubsystem(turret, () -> ,0).perpetually());
+        addTransformation(TurretState.ABSOLUTE, new MoveTalonSubsystem(turret, () -> setpoint.get() - drivetrain.getHandler().getYaw(), 0).perpetually();
     }
 
 }
