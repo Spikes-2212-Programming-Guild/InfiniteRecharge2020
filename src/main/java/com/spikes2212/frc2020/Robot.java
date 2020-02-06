3b15e7de8dd9d940f1a3cb22b9cf5e08118f5115
Ergoold <eran1gold@gmail.com>
2020-02-07 01:30:11 +0200

    reformat `Robot`

--- src/main/java/com/spikes2212/frc2020/Robot.java
+++ src/main/java/com/spikes2212/frc2020/Robot.java
@@ -1,15 +1,13 @@
 package com.spikes2212.frc2020;
 
-import com.spikes2212.frc2020.subsystems.Feeder;
-import com.spikes2212.frc2020.subsystems.Intake;
-import com.spikes2212.frc2020.subsystems.Shooter;
-import com.spikes2212.frc2020.subsystems.Turret;
+import com.spikes2212.frc2020.subsystems.*;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
 
     private static OI oi;
+
     private static Shooter shooter = Shooter.getInstance();
     private static Turret turret = Turret.getInstance();
     private static Feeder feeder = Feeder.getInstance();
