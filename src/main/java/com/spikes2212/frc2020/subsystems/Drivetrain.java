8ffbf507e1eef572420be77ef0b7e618c8a889c4
Ergoold <eran1gold@gmail.com>
2020-02-07 01:19:00 +0200

    reformat `Drivetrain`

--- src/main/java/com/spikes2212/frc2020/subsystems/Drivetrain.java
+++ src/main/java/com/spikes2212/frc2020/subsystems/Drivetrain.java
@@ -35,6 +35,7 @@ public class Drivetrain extends OdometryDrivetrain {
             PigeonWrapper imu = new PigeonWrapper(left);
             instance = new Drivetrain(left, right, leftEncoder, rightEncoder, imu);
         }
+
         return instance;
     }
 
@@ -50,7 +51,7 @@ public class Drivetrain extends OdometryDrivetrain {
         this.rightEncoder = rightEncoder;
         this.imu = imu;
         this.odometry = new OdometryHandler(leftEncoder::getDistance, rightEncoder::getDistance, imu::getY,
-                0,0);
+                0, 0);
     }
 
     @Override
