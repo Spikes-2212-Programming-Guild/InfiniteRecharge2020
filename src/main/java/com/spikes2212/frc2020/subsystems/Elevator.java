c55550597c0d7577b46284c8397715560a820c0a
talsi <tal.sitton04@gmail.com>
2020-02-07 03:06:23 +0200

    made Elevator implement TalonSubsystem

--- src/main/java/com/spikes2212/frc2020/subsystems/Elevator.java
+++ src/main/java/com/spikes2212/frc2020/subsystems/Elevator.java
@@ -1,7 +1,10 @@
 package com.spikes2212.frc2020.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.spikes2212.frc2020.utils.HallEffectCounter;
+import com.spikes2212.lib.command.genericsubsystem.TalonSubsystem;
 import com.spikes2212.lib.dashboard.Namespace;
 import com.spikes2212.lib.dashboard.RootNamespace;
 import com.spikes2212.frc2020.RobotMap;
@@ -10,10 +13,11 @@ import com.spikes2212.lib.control.FeedForwardSettings;
 import com.spikes2212.lib.control.PIDSettings;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Timer;
 
 import java.util.function.Supplier;
 
-public class Elevator extends GenericSubsystem {
+public class Elevator extends GenericSubsystem implements TalonSubsystem {
 
     private static final Namespace elevator = new RootNamespace("elevator");
 
@@ -21,6 +25,9 @@ public class Elevator extends GenericSubsystem {
     private static final Supplier<Double> kP = pidNamespace.addConstantDouble("kP", 0);
     private static final Supplier<Double> kI = pidNamespace.addConstantDouble("kI", 0);
     private static final Supplier<Double> kD = pidNamespace.addConstantDouble("kD", 0);
+    private static final Supplier<Double> waitTime = pidNamespace.addConstantDouble("wait time", 0);
+    private static final Supplier<Integer> timeout = pidNamespace.addConstantInt("timeout", 0);
+    public static Supplier<Double> setpoint = pidNamespace.addConstantDouble("setpoint", 0);
     public static final PIDSettings PID_SETTINGS = new PIDSettings(kP, kI, kD);
 
     private static final Namespace feedForwardNamespace = elevator.addChild("feed forward");
@@ -50,6 +57,8 @@ public class Elevator extends GenericSubsystem {
     private DigitalInput bottomHallEffect;
 
     private HallEffectCounter hallEffectCounter;
+
+    private double lastTimeNotOnTarget = 0;
 
     private Elevator(WPI_TalonSRX motor, Encoder encoder, DigitalInput bottomHallEffect, DigitalInput hallEffectCounter) {
         this.motor = motor;
@@ -94,5 +103,47 @@ public class Elevator extends GenericSubsystem {
         elevator.putNumber("encoder", encoder::get);
         elevator.putBoolean("bottom limit switch", bottomHallEffect::get);
         elevator.putNumber("top limit switch", hallEffectCounter::getCurrentMagnet);
+    }
+
+    @Override
+    public void configureLoop() {
+        motor.configFactoryDefault();
+        motor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, timeout.get());
+
+        motor.configNominalOutputForward(0, timeout.get());
+        motor.configNominalOutputReverse(0, timeout.get());
+        motor.configPeakOutputForward(1, timeout.get());
+        motor.configPeakOutputReverse(-1, timeout.get());
+
+        motor.configAllowableClosedloopError(0, 0, timeout.get());
+        motor.config_kP(0, kP.get(), timeout.get());
+        motor.config_kI(0, kI.get(), timeout.get());
+        motor.config_kD(0, kD.get(), timeout.get());
+    }
+
+    @Override
+    public void pidSet(double setpoint) {
+        motor.configPeakOutputForward(1, timeout.get());
+        motor.configPeakOutputReverse(-1, timeout.get());
+
+        motor.config_kP(0, kP.get(), timeout.get());
+        motor.config_kI(0, kI.get(), timeout.get());
+        motor.config_kD(0, kD.get(), timeout.get());
+
+        motor.set(ControlMode.Position, setpoint);
+
+    }
+
+    @Override
+    public void finish() {
+        stop();
+    }
+
+    @Override
+    public boolean onTarget(double setpoint) {
+        if (getCurrentMagnet().get() == setpoint)
+            lastTimeNotOnTarget = Timer.getFPGATimestamp();
+
+        return Timer.getFPGATimestamp() - lastTimeNotOnTarget >= waitTime.get();
     }
 }
