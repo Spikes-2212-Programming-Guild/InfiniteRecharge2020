fdb035aec7c8f6b16e6f1503691782997504a760
talsi <tal.sitton04@gmail.com>
2020-02-08 15:09:50 +0200

    added testing for IntakeFeederStateMachine

--- src/main/java/com/spikes2212/frc2020/statemachines/IntakeFeederStateMachine.java
+++ src/main/java/com/spikes2212/frc2020/statemachines/IntakeFeederStateMachine.java
@@ -1,6 +1,8 @@
 package com.spikes2212.frc2020.statemachines;
 
+import com.spikes2212.lib.dashboard.RootNamespace;
 import com.spikes2212.lib.state.StateMachine;
+import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 
 import static com.spikes2212.frc2020.statemachines.FeederStateMachine.FeederState;
@@ -8,8 +10,10 @@ import static com.spikes2212.frc2020.statemachines.IntakeStateMachine.IntakeStat
 
 public class IntakeFeederStateMachine extends StateMachine<IntakeFeederStateMachine.IntakeFeederState> {
 
+    public static RootNamespace intakeFeederStateNamespace = new RootNamespace("intake feeder state");
+
     public enum IntakeFeederState {
-        CLOSE_INTAKE_CLOSE_FEEDER, CLOSE_INTAKE_OPEN_FEEDER, OPEN_INTAKE_CLOSE_FEEDER, OPEN_INTAKE_OPEN_FEEDER,
+        OFF, FEED_TO_LEVEL_1, COLLECT, FEED_TO_SHOOTER
     }
 
     private static IntakeFeederStateMachine instance;
@@ -26,22 +30,31 @@ public class IntakeFeederStateMachine extends StateMachine<IntakeFeederStateMach
     private static FeederStateMachine feederFSM = FeederStateMachine.getInstance();
 
     private IntakeFeederStateMachine() {
-        super(IntakeFeederState.CLOSE_INTAKE_CLOSE_FEEDER);
+        super(IntakeFeederState.OFF);
     }
 
     @Override
     protected void generateTransformations() {
-        addTransformation(IntakeFeederState.CLOSE_INTAKE_OPEN_FEEDER,
+        addTransformation(IntakeFeederState.OFF,
+                new ParallelCommandGroup(intakeFSM.getTransformationFor(IntakeState.CLOSE),
+                    feederFSM.getTransformationFor(FeederState.OFF)
+        ));
+        addTransformation(IntakeFeederState.FEED_TO_LEVEL_1,
                 new ParallelCommandGroup(intakeFSM.getTransformationFor(IntakeState.CLOSE),
                         feederFSM.getTransformationFor(FeederState.FEED_TO_LVL_1)));
-        addTransformation(IntakeFeederState.CLOSE_INTAKE_CLOSE_FEEDER,
+        addTransformation(IntakeFeederState.FEED_TO_SHOOTER,
                 new ParallelCommandGroup(intakeFSM.getTransformationFor(IntakeState.CLOSE),
                         feederFSM.getTransformationFor(FeederState.FEED_TO_SHOOTER)));
-        addTransformation(IntakeFeederState.OPEN_INTAKE_OPEN_FEEDER,
-                new ParallelCommandGroup(intakeFSM.getTransformationFor(IntakeState.OPEN),
-                        feederFSM.getTransformationFor(FeederState.FEED_TO_LVL_1)));
-        addTransformation(IntakeFeederState.OPEN_INTAKE_CLOSE_FEEDER,
+        addTransformation(IntakeFeederState.COLLECT,
                 new ParallelCommandGroup(intakeFSM.getTransformationFor(IntakeState.OPEN),
                         feederFSM.getTransformationFor(FeederState.FEED_TO_SHOOTER)));
+    }
+
+    public void configureDashboard() {
+        intakeFeederStateNamespace.putString("state", getState()::name);
+        intakeFeederStateNamespace.putData("turn off", (Sendable)getTransformationFor(IntakeFeederState.OFF));
+        intakeFeederStateNamespace.putData("feed to level 1", (Sendable)getTransformationFor(IntakeFeederState.FEED_TO_LEVEL_1));
+        intakeFeederStateNamespace.putData("feed to shooter", (Sendable)getTransformationFor(IntakeFeederState.FEED_TO_SHOOTER));
+        intakeFeederStateNamespace.putData("collect", (Sendable)getTransformationFor(IntakeFeederState.COLLECT));
     }
 }
