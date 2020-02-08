609fff5d6264d262c9f9780de2c6c166008b8f40
talsi <tal.sitton04@gmail.com>
2020-02-08 11:53:42 +0200

    added stateMachine to Grip Command

--- src/main/java/com/spikes2212/frc2020/commands/Grip.java
+++ src/main/java/com/spikes2212/frc2020/commands/Grip.java
@@ -1,22 +1,28 @@
 package com.spikes2212.frc2020.commands;
 
+import com.spikes2212.frc2020.statemachines.IntakeFeederStateMachine;
 import com.spikes2212.frc2020.subsystems.Feeder;
 import com.spikes2212.frc2020.subsystems.Intake;
 import com.spikes2212.lib.command.genericsubsystem.commands.MoveGenericSubsystem;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-import java.util.function.Supplier;
+import static com.spikes2212.frc2020.statemachines.IntakeFeederStateMachine.IntakeFeederState;
 
 public class Grip extends SequentialCommandGroup {
 
     private Intake intake = Intake.getInstance();
     private Feeder feeder = Feeder.getInstance();
+    private IntakeFeederStateMachine intakeFeederFSM = IntakeFeederStateMachine.getInstance();
 
     public Grip() {
-        addCommands(new MoveGenericSubsystem(intake, intake.getGripSpeed()).withInterrupt
+        addCommands(
+                intakeFeederFSM.getTransformationFor(IntakeFeederState.OPEN_INTAKE_CLOSE_FEEDER),
+                new MoveGenericSubsystem(intake, intake.getGripSpeed()).withInterrupt
                         (() -> intake.getSuppliedCurrent() - intake.getStatorCurrent() >= intake.getCurrentLimit())
-                , ((new MoveGenericSubsystem(feeder, feeder::getProvidedSpeed)).deadlineWith
-                        (new MoveGenericSubsystem(intake, intake.getGripSpeed()))).withTimeout(feeder.getFeedTime()));
+                , intakeFeederFSM.getTransformationFor(IntakeFeederState.OPEN_INTAKE_OPEN_FEEDER),
+                ((new MoveGenericSubsystem(feeder, feeder::getProvidedSpeed)).deadlineWith
+                        (new MoveGenericSubsystem(intake, intake.getGripSpeed()))).withTimeout(feeder.getFeedTime())
+                , intakeFeederFSM.getTransformationFor(IntakeFeederState.CLOSE_INTAKE_CLOSE_FEEDER));
     }
 
 }
