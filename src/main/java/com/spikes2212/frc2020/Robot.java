795e41387becb96ffd16bcd4e0ab80e4170d4e37
YuvalLevy1 <42040830+YuvalLevy1@users.noreply.github.com>
2020-02-10 20:22:45 +0200

    Revert "Dev"

--- src/main/java/com/spikes2212/frc2020/Robot.java
+++ src/main/java/com/spikes2212/frc2020/Robot.java
@@ -1,33 +1,11 @@
 package com.spikes2212.frc2020;
 
-import com.spikes2212.frc2020.commands.Climb;
-import com.spikes2212.frc2020.subsystems.*;
+import edu.wpi.first.wpilibj.IterativeRobotBase;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
-
-    private static OI oi;
-
-    private static Shooter shooter = Shooter.getInstance();
-    private static Turret turret = Turret.getInstance();
-    private static Feeder feeder = Feeder.getInstance();
-    private static Intake intake = Intake.getInstance();
-    private static Climber climber = Climber.getInstance();
-
     @Override
     public void robotInit() {
-        shooter.configureDashboard();
-        turret.configureDashboard();
-        feeder.configureDashboard();
-        intake.configureDashboard();
-        climber.configureDashboard();
 
-        oi = new OI();
-    }
-
-    @Override
-    public void robotPeriodic() {
-        CommandScheduler.getInstance().run();
     }
 }
