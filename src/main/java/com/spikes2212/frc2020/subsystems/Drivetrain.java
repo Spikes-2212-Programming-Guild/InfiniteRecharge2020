78496324d4e01a4f35a4d3e1414594a1abcb8ba4
tuval88 <ttuval6677@gamil.com>
2020-02-14 17:09:06 +0200

    reformatted the code

--- src/main/java/com/spikes2212/frc2020/subsystems/Drivetrain.java
+++ src/main/java/com/spikes2212/frc2020/subsystems/Drivetrain.java
@@ -24,23 +24,25 @@ public class Drivetrain extends OdometryDrivetrain {
 
     public static Drivetrain getInstance() {
         if(instance == null) {
-            WPI_VictorSPX leftVictor, rightVictor;
-            WPI_TalonSRX left = new WPI_TalonSRX(RobotMap.CAN.DRIVETRAIN_LEFT_TALON);
-            WPI_TalonSRX right = new WPI_TalonSRX(RobotMap.CAN.DRIVETRAIN_RIGHT_TALON);
-            (leftVictor = new WPI_VictorSPX(RobotMap.CAN.DRIVETRAIN_LEFT_VICTOR)).follow(left);
-            (rightVictor = new WPI_VictorSPX(RobotMap.CAN.DRIVETRAIN_RIGHT_VICTOR)).follow(right);
-            right.setInverted(true);
+            WPI_TalonSRX leftTalon = new WPI_TalonSRX(RobotMap.CAN.DRIVETRAIN_LEFT_TALON);
+            WPI_TalonSRX rightTalon = new WPI_TalonSRX(RobotMap.CAN.DRIVETRAIN_RIGHT_TALON);
+            WPI_VictorSPX leftVictor = new WPI_VictorSPX(RobotMap.CAN.DRIVETRAIN_LEFT_VICTOR);
+            WPI_VictorSPX rightVictor = new WPI_VictorSPX(RobotMap.CAN.DRIVETRAIN_RIGHT_VICTOR);
+            leftVictor.follow(leftTalon);
+            rightVictor.follow(rightTalon);
+            rightTalon.setInverted(true);
             rightVictor.setInverted(true);
             Encoder leftEncoder = new Encoder(RobotMap.DIO.DRIVETRAIN_LEFT_ENCODER_POS, RobotMap.DIO.DRIVETRAIN_LEFT_ENCODER_NEG);
             Encoder rightEncoder = new Encoder(RobotMap.DIO.DRIVETRAIN_RIGHT_ENCODER_POS, RobotMap.DIO.DRIVETRAIN_RIGHT_ENCODER_NEG);
             leftEncoder.setDistancePerPulse(wheelDiameter.get() * 0.0254 * Math.PI / 360);
             rightEncoder.setDistancePerPulse(wheelDiameter.get() * 0.0254 * Math.PI / 360);
-            PigeonWrapper imu = new PigeonWrapper(left);
-            instance = new Drivetrain(left, right, leftEncoder, rightEncoder, imu, leftVictor, rightVictor);
+            PigeonWrapper imu = new PigeonWrapper(leftTalon);
+            instance = new Drivetrain(leftTalon, rightTalon, leftVictor, rightVictor, leftEncoder, rightEncoder, imu);
         }
 
         return instance;
     }
+
     private WPI_VictorSPX leftVictor, rightVictor;
     private Encoder leftEncoder;
     private Encoder rightEncoder;
@@ -48,16 +50,16 @@ public class Drivetrain extends OdometryDrivetrain {
     private OdometryHandler odometry;
     private boolean inverted;
 
-    private Drivetrain(SpeedController left, SpeedController right, Encoder leftEncoder, Encoder rightEncoder,
-                       PigeonWrapper imu, WPI_VictorSPX leftVictor, WPI_VictorSPX rightVictor) {
+    private Drivetrain(WPI_TalonSRX left, WPI_TalonSRX right, WPI_VictorSPX leftVictor, WPI_VictorSPX rightVictor,
+                       Encoder leftEncoder, Encoder rightEncoder, PigeonWrapper imu) {
         super(left, right);
+        this.leftVictor = leftVictor;
+        this.rightVictor = rightVictor;
         this.leftEncoder = leftEncoder;
         this.rightEncoder = rightEncoder;
         this.imu = imu;
         this.odometry = new OdometryHandler(leftEncoder::getDistance, rightEncoder::getDistance, imu::getY,
                 0, 0);
-        this.leftVictor = leftVictor;
-        this.rightVictor = rightVictor;
     }
 
     @Override
