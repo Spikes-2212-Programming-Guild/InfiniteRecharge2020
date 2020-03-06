70c69d33f55d72bbc117dd03f2a7700776b0337c
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2020-03-06 07:59:48 +0200

    renamed variables in MoveTurretToFieldRelativeAngle

--- src/main/java/com/spikes2212/frc2020/commands/MoveTurretToFieldRelativeAngle.java
+++ src/main/java/com/spikes2212/frc2020/commands/MoveTurretToFieldRelativeAngle.java
@@ -2,7 +2,6 @@ package com.spikes2212.frc2020.commands;
 
 import com.spikes2212.frc2020.subsystems.Drivetrain;
 import com.spikes2212.frc2020.subsystems.Turret;
-import com.spikes2212.lib.command.RepeatCommand;
 import com.spikes2212.lib.command.genericsubsystem.commands.MoveTalonSubsystem;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -13,19 +12,19 @@ public class MoveTurretToFieldRelativeAngle extends SequentialCommandGroup {
     private Drivetrain drivetrain = Drivetrain.getInstance();
 
     private double targetTurretYaw = 0;
-    private double initialDrivetrainYaw = 0;
+    private double drivetrainOffsetYaw = 0;
 
     public MoveTurretToFieldRelativeAngle() {
         addCommands(
                 new InstantCommand(() -> {
                     targetTurretYaw = turret.getYaw();
-                    initialDrivetrainYaw = drivetrain.getYaw();
+                    drivetrainOffsetYaw = drivetrain.getYaw();
                 }),
                 new MoveTalonSubsystem(turret, this::calculateSetpoint, () -> 0.0).perpetually()
         );
     }
 
     private double calculateSetpoint() {
-        return targetTurretYaw - (drivetrain.getYaw() - initialDrivetrainYaw);
+        return targetTurretYaw - (drivetrain.getYaw() - drivetrainOffsetYaw);
     }
 }
