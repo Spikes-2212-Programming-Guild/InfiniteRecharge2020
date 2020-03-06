029d3028b479a3c691fdb01080d33081639aac3b
Matangn13 <matangn13@gmail.com>
2020-03-06 14:22:55 +0200

    added Encoder to Feeder

--- src/main/java/com/spikes2212/frc2020/subsystems/Feeder.java
+++ src/main/java/com/spikes2212/frc2020/subsystems/Feeder.java
@@ -5,6 +5,7 @@ import com.spikes2212.frc2020.RobotMap;
 import com.spikes2212.lib.command.genericsubsystem.GenericSubsystem;
 import com.spikes2212.lib.command.genericsubsystem.commands.MoveGenericSubsystem;
 import com.spikes2212.lib.dashboard.RootNamespace;
+import edu.wpi.first.wpilibj.Encoder;
 
 import java.util.function.Supplier;
 
@@ -17,6 +18,7 @@ public class Feeder extends GenericSubsystem {
     public static final Supplier<Double> speed = feederNamespace.addConstantDouble("speed", 0.7);
     public static final Supplier<Double> feedTimeLimit = feederNamespace.addConstantDouble("feeding time", 2);
 
+    private static final double distancePerPulse = 1 / 1024.0;
     private static final Feeder instance = new Feeder();
 
     public static Feeder getInstance() {
@@ -25,6 +27,8 @@ public class Feeder extends GenericSubsystem {
 
     private WPI_VictorSPX motor;
 
+    private Encoder encoder;
+
     private boolean enabled;
 
     private boolean isOpen = true;
@@ -32,6 +36,8 @@ public class Feeder extends GenericSubsystem {
     private Feeder() {
         super(minSpeed, maxSpeed);
         motor = new WPI_VictorSPX(RobotMap.CAN.FEEDER_VICTOR);
+        encoder = new Encoder(RobotMap.DIO.FEEDER_ENCODER_POS, RobotMap.DIO.FEEDER_ENCODER_NEG);
+        encoder.setDistancePerPulse(distancePerPulse);
         enabled = true;
     }
 
